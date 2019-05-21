a4dadbb3d30f83559ccaf4c09cea763784898107
Fernando Mayer <fernandomayer@gmail.com>
2019-05-21 19:53:20 -0300

    Inclui porta no deploy

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,8 @@
 USER=fernandomayer
 HOST=leg.ufpr.br
 DIR=public_html/pesqrep
+read -p 'PORTA: ' PORT
 
-Rscript -e 'rmarkdown::render("pesqrep.Rmd")' && rsync -avz pesqrep.html ${USER}@${HOST}:~/${DIR}/index.html
+Rscript -e 'rmarkdown::render("pesqrep.Rmd")' && rsync -avz -e "ssh -p $PORT" pesqrep.html ${USER}@${HOST}:~/${DIR}/index.html
 
 exit 0
