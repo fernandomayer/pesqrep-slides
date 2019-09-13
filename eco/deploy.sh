92b1001a237733aa1681818329ae9c507e10024a
Fernando Mayer <fernandomayer@gmail.com>
2019-09-13 02:00:27 -0300

    Inclui PDF

--- eco/deploy.sh
+++ eco/deploy.sh
@@ -5,6 +5,8 @@ HOST=leg.ufpr.br
 DIR=public_html/pesqrep-eco
 read -p 'PORTA: ' PORT
 
-Rscript -e 'rmarkdown::render("pesqrep-eco.Rmd")' && rsync -avz -e "ssh -p $PORT" pesqrep-eco.html ${USER}@${HOST}:~/${DIR}/index.html
+Rscript -e 'rmarkdown::render("pesqrep-eco.Rmd")'
+rsync -avz -e "ssh -p $PORT" pesqrep-eco.html ${USER}@${HOST}:~/${DIR}/index.html
+rsync -avz -e "ssh -p $PORT" pesqrep-eco.pdf ${USER}@${HOST}:~/${DIR}/pesqrep-eco.pdf
 
 exit 0
