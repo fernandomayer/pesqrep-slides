dbb094d3f1b0bca1ba11592ea337a76b076a8573
Fernando Mayer <fernandomayer@gmail.com>
2021-06-01 03:13:28 -0300

    Adiciona xaringanBuilder

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ DIR=public_html/pesqrep
 read -p 'PORTA: ' PORT
 
 Rscript -e 'rmarkdown::render("pesqrep.Rmd")'
+Rscript -e 'xaringanBuilder::build_pdf("pesqrep.Rmd")'
 rsync -avz -e "ssh -p $PORT" pesqrep.html ${USER}@${HOST}:~/${DIR}/index.html
 rsync -avz -e "ssh -p $PORT" pesqrep.pdf ${USER}@${HOST}:~/${DIR}/pesqrep.pdf
 
