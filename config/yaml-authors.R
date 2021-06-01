2ef61bddd6c0a1ef195446c1f264e2f7741f028f
Fernando Mayer <fernandomayer@gmail.com>
2021-05-31 23:09:14 -0300

    Nova formatacao e unificacao com o eco

--- config/yaml-authors.R
+++
@@ -1,9 +0,0 @@
-if (knitr::opts_knit$get("rmarkdown.pandoc.to") %in% c("html", "slidy")) {
-    cat("  | Fernando P. Mayer         |\n",
-        "  |:-------------------------:|\n",
-        "  | `fernando.mayer@ufpr.br`  |\n",
-        "  |  LEG/DEST/UFPR            |\n",
-        sep = "")
-} else {
-    cat("  Fernando P. Mayer\n")
-}
--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,11 @@
 
 USER=fernandomayer
 HOST=leg.ufpr.br
-DIR=public_html/pesqrep
+DIR=public_html/pesqrep-eco
 read -p 'PORTA: ' PORT
 
-Rscript -e 'rmarkdown::render("pesqrep.Rmd")' && rsync -avz -e "ssh -p $PORT" pesqrep.html ${USER}@${HOST}:~/${DIR}/index.html
+Rscript -e 'rmarkdown::render("pesqrep-eco.Rmd")'
+rsync -avz -e "ssh -p $PORT" pesqrep-eco.html ${USER}@${HOST}:~/${DIR}/index.html
+rsync -avz -e "ssh -p $PORT" pesqrep-eco.pdf ${USER}@${HOST}:~/${DIR}/pesqrep-eco.pdf
 
 exit 0
